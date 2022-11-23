7434f74678f025690a802ceaa7a594d68e85eb48
Dmitriy <zdv9997@mail.ru>
2022-11-23 10:51:19 +0200

    возводим число А в стпень И

---
+++ zad25/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int result = numA;
+for (int i = 2; i <= numB; i++)
+    result = result * numA;
+Console.WriteLine($"Значение {numA} в {numB} степени равно: {result}");
